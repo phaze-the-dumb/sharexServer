01ac5823f7f61786a07f64ff6a90f7bb9278e567
wiresboy-exe <57566773+wiresboy-exe@users.noreply.github.com>
2021-05-31 14:38:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require("express")
+var multer = require("multer");
+var body_parser = require("body-parser");
+var cors = require("cors");
+var keys = require('./keys.json')
+var app = express()
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "Uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+var Upload = multer({ storage: storage });
+
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(cors.default());
+
+app.use(express.static("Uploads"));
+
+app.get("/", function (req, res) { return res.redirect('https://wiresdev.ga')});
+
+app.post("/upload", Upload.single("file"), function (req, res) {
+    if(keys.includes(req.headers['key'])){
+        res.send("https://i.wiresdev.ga/" + req.file.originalname)
+        console.log("Yay uploaded "+req.file.originalname)
+    } else{
+        res.sendStatus(401).send('Error: Unauthorized')
+    }
+});
+
+app.listen(3854, function () {
+    console.log("App started on port 3000");
+});
